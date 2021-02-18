8c95e5fc312c1830e51510f7453a7e2e42104055
Bryan Bush <bryan@dialexa.com>
2021-02-17 23:43:36 -0600

    Initial setup.

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Bachelor Fantasy League</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
