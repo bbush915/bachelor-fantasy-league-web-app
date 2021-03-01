cb15f1a1b16c0f090f211c647a701af4612f9018
Kim Kline <kim@dialexa.com>
2021-03-01 17:26:01 -0600

    search icon

--- src/components/pages/JoinLeague/index.vue
+++ src/components/pages/JoinLeague/index.vue
@@ -3,22 +3,32 @@
     <h1 class="text-white mb-8">Join a League</h1>
     <div class="bg-gray-dark rounded-xl px-6 py-6">
       <h2 class="text-white mb-4">Search</h2>
-      <input
-        class="bg-gray-dark border border-white rounded-lg"
-        aria-label="League Name"
-      />
-      <button class="w-8 h-8 ml-2 rounded-full bg-pink text-gray-dark">
-        Search
-      </button>
+      <div class="flex flex-col">
+        <label class="font-thin">League Name</label>
+        <div class="flex items-center mb-6">
+          <input
+            class="bg-gray-dark border border-white rounded-lg px-2 h-8"
+            aria-label="League Name"
+          />
+
+          <button class="w-8 h-8 ml-2 rounded-full bg-pink text-gray-dark px-1">
+            <SearchIcon />
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import SearchIcon from '@/assets/search.svg';
 
 const JoinLeague = defineComponent({
   name: 'JoinLeague',
+  components: {
+    SearchIcon,
+  },
 });
 
 export default JoinLeague;
